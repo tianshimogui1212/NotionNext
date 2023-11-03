d03168a08122754d22adbdfc74aea7aef8121856
tianshimogui1212 <141617788+tianshimogui1212@users.noreply.github.com>
2023-11-03 18:59:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'yumo', // 您的昵称 例如 Wuqing
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Wuqing', // 您的昵称 例如 Wuqing
   BIO: process.env.NEXT_PUBLIC_BIO || '我喜欢天空，喜欢红霞满天，也喜欢烟雨天青，更喜欢满天繁星', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Huangyulin, 博客', // 网站关键词 英文逗号隔开
